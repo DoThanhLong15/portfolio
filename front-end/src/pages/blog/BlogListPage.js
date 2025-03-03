b67b487bbe9be77f0455df6bd8d4bb9a7b596fc2
DoThanhLong15 <108256260+DoThanhLong15@users.noreply.github.com>
2025-03-03 17:13:27 +0700

    add blog detail page, split to small component

--- front-end/src/pages/blog/BlogListPage.js
+++ front-end/src/pages/blog/BlogListPage.js
@@ -1,24 +1,23 @@
 import { useState } from "react";
 
-import TitleSession from "../../components/others/TitleSession";
+import BlogPaginator from "../../components/blog/BlogPaginator";
 import PageDescription from "../../components/others/PageDescription";
-import SmallBlogItem from "../../components/blog/SmallBlogItem";
+import TitleSession from "../../components/others/TitleSession";
 
 import data from "./data/data";
 
 const BlogListPage = () => {
   const [description] = useState(data.pageDescription);
-  const [blogs, setBlogs] = useState(data.blogs);
-
-  console.log(data);
+  const [blogs] = useState(data.blogs);
+  const [blogPerPage] = useState(5);
 
   return (
     <>
-      <TitleSession title="Blog" />
+      <TitleSession title="Blog"/>
 
       <PageDescription description={description} />
 
-      {blogs && blogs.map((blogItem) => <SmallBlogItem blog={blogItem} key={blogItem.id} />)}
+      <BlogPaginator blogs={blogs} itemsPerPage={blogPerPage} />
     </>
   );
 };
--- front-end/src/routes/index.js
+++ front-end/src/routes/index.js
@@ -2,16 +2,15 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-// Import pages (or components)
 import Header from "../layout/header/Header";
 import Footer from "../layout/footer/Footer";
 
-import HomePage from "../pages/home/HomePage";
-
 import AboutMePage from "../pages/about-me/AboutMePage";
+import BlogDetailPage from "../pages/blog/BlogDetailPage";
 import BlogListPage from "../pages/blog/BlogListPage";
 import ContactPage from "../pages/contact/ContactPage";
 import ProjectListPage from "../pages/project/ProjectListPage";
+import HomePage from "../pages/home/HomePage";
 
 const MainRoute = () => {
   return (
@@ -23,9 +22,14 @@ const MainRoute = () => {
           <div className="content-area">
             <Routes>
               <Route exact path="/" element={<HomePage />} />
+
               <Route exact path="/blog" element={<BlogListPage />} />
+              <Route exact path="/blog/:slug" element={<BlogDetailPage />} />
+
+
               <Route exact path="/about-me" element={<AboutMePage />} />
               <Route exact path="/projects" element={<ProjectListPage />} />
+
               <Route exact path="/contact" element={<ContactPage />} />
             </Routes>
           </div>
