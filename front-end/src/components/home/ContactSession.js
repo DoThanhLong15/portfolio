eb082646360d287549334f0578cdfcbeefa416b1
DoThanhLong15 <108256260+DoThanhLong15@users.noreply.github.com>
2025-03-06 17:23:23 +0700

    add ContactPage, contact.json, fix and add new icon in Footer, fix about-me.json

--- front-end/src/components/home/ContactSession.js
+++ front-end/src/components/home/ContactSession.js
@@ -1,21 +1,13 @@
+import ContactContentArea from "./../contact/ContactContentArea";
 import TitleSession from "../others/TitleSession";
-import style from "./style/ContactSession.module.css";
+
 
 const ContactSession = () => {
   return (
     <>
       <TitleSession title="contacts" />
 
-      <div className="flex">
-        <div className={style.description}>
-          I'm interested in freelance opportunities. However, if you have other
-          request or question, don't hesitate to contact me.
-        </div>
-        <div className={style.contact}>
-          <div className={style.contactTitle}>Message me here</div>
-          <div className={style.contactItem}>longd8833@gmail.com</div>
-        </div>
-      </div>
+      <ContactContentArea />
     </>
   );
 };
--- front-end/src/layout/footer/Footer.js
+++ front-end/src/layout/footer/Footer.js
@@ -1,22 +1,32 @@
-import "./Footer.css";
+import data from "./../../data/contact.json";
+import style from "./Footer.module.css";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   return (
     <>
-      <div className="footer pos-relative">
-        <div className="line pos-absolute"></div>
-        <div className="content flex">
-          <div className="description-area">
-            <div className="flex">
-              <h3 className="title">Long</h3>
-              <div className="email">longd8833@gmail.com</div>
+      <div className={style.footer}>
+        <div className={style.line}></div>
+        <div className={style.content}>
+          <div className={style.descriptionArea}>
+            <div className={style.authorArea}>
+              <h3 className={style.author}>Long</h3>
+              <div className={style.email}>longd8833@gmail.com</div>
             </div>
-            <p className="description">Backend developer</p>
+            <p className={style.description}>Backend developer</p>
           </div>
 
-          <div className="media">
-            <h3 className="title">Media</h3>
-            <p>Icon</p>
+          <div className={style.media}>
+            <h3 className={style.title}>Media</h3>
+            <a
+              href={data.contact.github}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faGithub} className={style.icon}/>
+            </a>
           </div>
         </div>
 
